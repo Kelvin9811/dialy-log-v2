a99dee4464b99aa826c1f58239b1c36e55f204e0
Kelvin9811 <kelvin.ortiz@banistmo.com>
2026-04-16 19:20:08 -0500

    Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "dailylogv22015003220150032": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
